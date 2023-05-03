a3ee2246339ce6b04d448ddad969ca347c9eb623
Filip <filip.tokar@seznam.cz>
2023-05-03 11:15:23 +0200

    fixing header routing

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import LibraryLogo from "../assets/favpng_movie-logo.png"
+import { Link } from "react-router-dom";
+import Movies from "../Pages/Movies";
 
 function Nav({blue}) {
   return (
     <nav>
       <div className={`nav__container ${blue && " nav__container--blue"}`}>
+        <Link to="/">
         <figure>
           <img className="logo" src={LibraryLogo} alt="" />
         </figure>
+        </Link>
+        
         <ul className="nav__link--list">
           <li className="nav__link">
-            <a href="/" className="nav__link--anchor link__hover-effect no-click ">
-                Home
-            </a>
+          <Link to="/" className="nav__link--anchor link__hover-effect">
+            Home
+          </Link>
             
           </li>
           <li className="nav__link">
-          <a href="/" className="nav__link--anchor link__hover-effect no-click ">
-                Browse
-            </a>
-            
+          <Link to="/movies" className="nav__link--anchor link__hover-effect">
+            Browse
+          </Link>
           </li>
           <li className="nav__link">
-          <a href="/" className="nav__link--btn no-click">
-                Contact
-            </a>
-            
+            <Link to="#" className="nav__link--btn no-click">
+              Contact
+            </Link>  
           </li>
         </ul>
       </div>
